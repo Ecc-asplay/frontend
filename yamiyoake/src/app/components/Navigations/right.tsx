9b27e3e624bc553c5c7c2340852f2aece0b61a85
SFjapan <fs12265669@gmail.com>
2024-11-08 15:28:37 +0900

    メインページ仮完成

---
+++ yamiyoake/src/app/components/Navigations/right.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import happa from "@/app/img/happa.png";
+import pencil from "@/app/img/ペン.png";
+const RightNavigation = () =>{
+    return(
+        <div className="bg-[url('img/mokume.png')] bg-repeat-round w-[20%] h-screen relative">
+            <div className="absolute top-0 left-7">
+                <Image src={happa} alt="happa" width={50} className="h-full" />
+            </div>
+            <div className="absolute bottom-0 right-7 transition hover:-translate-y-4 hover:duration-200">
+                <Image src={pencil} alt="happa" width={50} className="h-full" />
+            </div>
+        </div>
+    );
+}
+export {RightNavigation}
