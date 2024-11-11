d1e535ca7453e82735a4c136f62d87c593aaf551
SFjapan <fs12265669@gmail.com>
2024-11-11 15:34:38 +0900

    投稿詳細仮完成

---
+++ yamiyoake/src/app/components/comments.tsx
@@ -0,0 +1,47 @@
+import { testData,comments } from "../test_data";
+import sippo from "@/app/img/sippo.png";
+import sippo_reply from "@/app/img/sippo_reply.png"
+import send from "@/app/img/send-svgrepo-com.png";
+import Image from "next/image";
+interface PostID{
+    post_id:string|null
+}
+const Comments:React.FC<PostID> = ({post_id}) =>{
+    const post = testData.find(e=>e.post_id===post_id);
+    const post_comments = comments.filter(e=>e.post_id===post_id);
+    return(
+        <div className="flex flex-col w-[20%] h-screen  bg-[url('img/mokume.png')] p-3 items-center relative">
+            {post_comments?(
+                <div className="object-cover w-full h-full flex flex-col items-center gap-10 hidden-scrollbar overflow-auto">
+                    {post_comments.map((e)=>(
+                        <div key={e.comment_id} className="object-cover w-[90%] relative ">
+                            {e.user_id ===post?.user_id?(
+                                <div className=" bg-[#A5BBA2] rounded-lg p-3 ">
+                                    <p>{e.comments}</p>
+                                    <Image src={sippo} width={30} height={30} alt="sippo" className="absolute -bottom-3 right-0"/>
+                                </div>):
+                                (<div className="bg-[#B8A193] rounded-lg p-3">
+                                    <p>{e.comments}</p>
+                                    <Image src={sippo_reply} width={30} height={30} alt="sippo" className="absolute -bottom-3 left-0"/>
+                                </div>)
+                            }
+                            
+                        </div>
+                    ))}
+                </div>
+                ):(
+                    <div>
+                        error
+                    </div>
+                )
+            }
+            <div className="absolute bottom-3 left-1/2 w-[90%] transition -translate-x-1/2 rounded-xl bg-[#DCD5CD] flex justify-center">
+                <input type="text" className="bg-[#DCD5CD] outline-none" />
+                <button><Image src={send} alt="send" width={30} height={30} /></button>
+            </div>
+            
+        </div>
+        
+    )
+}
+export {Comments}
---
+++ yamiyoake/src/app/reactions.ts
@@ -0,0 +1,19 @@
+import neutral from "@/app/img/reactions/face-neutral-svgrepo-com.png";
+import funny from "@/app/img/reactions/emoji-emoticon-funny-svgrepo-com.png";
+import frown from "@/app/img/reactions/face-frown-svgrepo-com.png";
+import smile from "@/app/img/reactions/emoji-smile-svgrepo-com.png";
+import sad from "@/app/img/reactions/sad-squint-svgrepo-com.png";
+
+import color_neutral from "@/app/img/reactions/color_neatural.png";
+import color_funny from "@/app/img/reactions/color_funny.png";
+import color_frown from "@/app/img/reactions/color_frown.png";
+import color_smile from "@/app/img/reactions/color_smile.png";
+import color_sad from "@/app/img/reactions/color_sad.png";
+
+export const reaction_icons = [
+    funny,smile,neutral,frown,sad
+];
+
+export const color_reactions = [
+    color_funny,color_smile,color_neutral,color_frown,color_sad
+]
