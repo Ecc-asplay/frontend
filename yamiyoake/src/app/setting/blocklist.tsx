1a00236777e083132bbddb434c4f4a9dba31236c
uhaii <fanweiwei315@gmail.com>
2024-12-16 14:49:25 +0900

    設定ページ関連通知設定、ブロックリスト、お問い合わせ画面更新（未完成）

--- yamiyoake/src/app/setting/blocklist.tsx
+++ yamiyoake/src/app/setting/blocklist.tsx
@@ -12,52 +12,52 @@ const Blocklist: React.FC = () => {
     const [ confirmUnblockId, setConfirmUnblockId ] = useState<string|null>(null)
     
     // リスト取得
-    useEffect(() => {
-        const fetchList = async () => {
-            try {
-                const response = await axios.get<BlocklistData[]>("api?/blocklist")
-                // データ取得
-                setList(response.data)
+    // useEffect(() => {
+    //     const fetchList = async () => {
+    //         try {
+    //             const response = await axios.get<BlocklistData[]>("api?/blocklist")
+    //             // データ取得
+    //             setList(response.data)
 
-            } catch (error){
-                console.error("Error: ", error);
-            } finally {
-                setLoading(false)
-            }
-            alert("Error！");
-        }
-        fetchList()
-    }, []);
+    //         } catch (error){
+    //             console.error("Error: ", error);
+    //         } finally {
+    //             setLoading(false)
+    //         }
+    //         alert("Error！");
+    //     }
+    //     fetchList()
+    // }, []);
 
-    // ブロック解除
-    const handleUnblock = async (id: string) => {
-        try {
-            await axios.delete("api/blocklist",{data:{id}} as any)
-            setList((prev) => prev.filter((index) => index.id != id))
-        } catch (error) {
-            console.error("Error: ", error);
-        } finally {
-            setConfirmUnblockId(null)
-        }
-    }
+    // // ブロック解除
+    // const handleUnblock = async (id: string) => {
+    //     try {
+    //         await axios.delete("api/blocklist",{data:{id}} as any)
+    //         setList((prev) => prev.filter((index) => index.id != id))
+    //     } catch (error) {
+    //         console.error("Error: ", error);
+    //     } finally {
+    //         setConfirmUnblockId(null)
+    //     }
+    // }
 
     return (
-        <div className="flex flex-col items-center px-6 py-12">
-            <h1 className="text-basetext font-extrabold">ブロックリスト</h1>
+        <div className="flex flex-col px-6 py-12">
+            <h1 className="text-middlebrown font-bold ml-2">ブロックリスト</h1>
             {/* リスト */}
             <table className="table-auto w-full">
-                <thead>
+                {/* <thead>
                     <tr className="text-middlebrown uppercase font-semibold">
                         <th className="py-3 px-6 text-center">ユーザーID</th>
                         <th className="py-3 px-6 text-center">ブロック時間</th>
                         <th></th>
                     </tr>
-                </thead>
+                </thead> */}
                 <tbody className="text-basetext">
                     {blocklist.map((index) => (
                         <tr key={index.id} className="border-b border-middlebrown">
-                            <td className="text-left py-3 px-6">{index.id}</td>
-                            <td className="text-center py-3 px-6">{index.blockedAt}</td>
+                            <td className="text-left py-3 px-6">ユーザ：{index.id}</td>
+                            {/* <td className="text-center py-3 px-6">{index.blockedAt}</td> */}
                             <td className="text-center py-3 px-6">
                                 <button onClick={() => setConfirmUnblockId(index.id)}
                                     className="bg-red-600 text-white px-4 py-1 rounded">
