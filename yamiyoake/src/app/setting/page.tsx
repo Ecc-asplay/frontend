87dd58cf8800ca8e0de6d182ccb75610cc1d2db4
SFjapan <fs12265669@gmail.com>
2025-01-23 15:15:11 +0900

    設定画面にナヴィゲーション追加

--- yamiyoake/src/app/setting/page.tsx
+++ yamiyoake/src/app/setting/page.tsx
@@ -4,28 +4,33 @@ import SideBar from "./SideBar";
 import SideBarSelected from "./SideBarSelected";
 import NotiDetailPush from "./noti-detail-push";
 import NotiDetailEmail from "./noti-detail-email";
-
+import { LeftNavigation } from "../components/navigations/left";
+import { RightNavigation } from "../components/navigations/right";
 const Setting: React.FC = () => {
     const [activeKey, setActiveKey] = useState("");
 
     return(
-        <div className="flex flex-col bg-basebg min-h-screen">
+        <div className="flex bg-basebg min-h-screen">
+            <LeftNavigation />
             {/* ヘッダ */}
-            <div className="fixed top-0 w-full h-8 bg-headerbrown" />
+            <div className="w-[60%]">
+                <div className="fixed top-0 w-full h-8 bg-headerbrown" />
 
-            {/* サイドバー */}
-            <div className="flex mt-4 p-0 m-0 h-full">
-                <SideBar onselect={setActiveKey}/>
-                <div className="min-w-[700px] p-5">
-                    {/* {SideBarSelected[activeKey]} */}
-                    {activeKey === "notiDetailPush" ? (
-                        <NotiDetailPush onselect={setActiveKey} />
-                    ): activeKey === "notiDetailEmail" ?(
-                        <NotiDetailEmail onselect={setActiveKey} />
-                    ): (SideBarSelected(setActiveKey)[activeKey]
-                    )}
+                {/* サイドバー */}
+                <div className="flex mt-4 p-0 m-0 h-full">
+                    <SideBar onselect={setActiveKey}/>
+                    <div className="min-w-[700px] p-5">
+                        {/* {SideBarSelected[activeKey]} */}
+                        {activeKey === "notiDetailPush" ? (
+                            <NotiDetailPush onselect={setActiveKey} />
+                        ): activeKey === "notiDetailEmail" ?(
+                            <NotiDetailEmail onselect={setActiveKey} />
+                        ): (SideBarSelected(setActiveKey)[activeKey]
+                        )}
+                    </div>
                 </div>
             </div>
+            <RightNavigation />
         </div>
     )
 }
